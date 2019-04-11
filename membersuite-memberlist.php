335eb717cbd851a09f1fb1a4d9369df478aaedc4
Wayne Graham <wgraham@clir.org>
2019-04-11 09:03:34 -0400

    Adds default template

---
+++ membersuite-memberlist.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:     Membersuite Memberlist
+ * Plugin URI:      https://github.com/clirdlf/membersuite-memberlist
+ * Description:     Membersuite membership list for WordPress
+ * Author:          Wayne Graham
+ * Author URI:      https://www.clir.org
+ * Text Domain:     clir
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Membersuite_Memberlist
+ */
+/**
+ * This is just a sample plugin.
+ */
+function membersuite_memberlist()
+{
+    echo 'Do nothing';
+}
